5e30ad3e913011a7f09dc553cc654597af9e084a
lornahokoth <60718374+lornahokoth@users.noreply.github.com>
2021-03-14 22:09:17 -0400

    Update signup.php

--- CSC 4370 - Web Programming/HW/nerdieluv/signup.php
+++ CSC 4370 - Web Programming/HW/nerdieluv/signup.php
@@ -7,50 +7,57 @@
     <br>
     <label>Where meek geeks meet!</label>
 </header>
-<fieldset>
-    <legend>
-        New User Signup:
-    </legend>
-    <form action="./signup-submit.php" method="POST">
-        <dl>
+
+<body>
+    <fieldset>
+        <legend>
+            New User Signup:
+        </legend>
+        <form action="./signup-submit.php" method="POST">
+
             <label><strong>Name:</strong></label>
             <div>
                 <input type="text" name="name" size="16" />
             </div>
-
+            <br>
             <label><strong>Gender:</strong></label>
             <div>
-                <input type="radio" name="gender" id="male" value="Male" />
+                <input type="radio" name="gender" id="male" value="M" />
                 <label for="male"> Male </label>
-                <input type="radio" name="gender" id="female" value="Female" />
+                <input type="radio" name="gender" id="female" value="F" checked />
                 <label for="female"> Female </label>
             </div>
+            <br>
             <label><strong>Age:</strong></label>
             <div>
-                <input type="number" name="age" maxlength="2" size="6" />
+                <input type="text" name="age" maxlength="2" size="6" />
             </div>
+            <br>
             <label><strong>Personality Type:</strong></label>
             <div>
-                <input type="text" name="name" maxlength="4" size="6" />
+                <input type="text" name="personality" maxlength="4" size="6" />
+                <label for="personality"> (<a href="http://www.humanmetrics.com/cgi-win/JTypes2.asp">Don't know your type?</a>)</label>
             </div>
+            <br>
             <label><strong>Favorite OS:</strong></label>
             <div>
-                <select name="section" id="class">
+                <select name="OS" id="class">
                     <option value="WS">Windows</option>
                     <option value="MS">Mac OS X</option>
                     <option value="LX">Linux</option>
-
                 </select>
             </div>
-            <label><strong>Seeking age:</strong></label>
+            <br>
+            <div>
+                <label><strong>Seeking age:</strong></label>
                 <input type="text" placeholder="min" name="minAge" maxlength="2" size="6" />
                 <label for="to"> to </label>
-                <input type="text" placeholder="max" name="maxAge" maxlength="2"  size="6" />
+                <input type="text" placeholder="max" name="maxAge" maxlength="2" size="6" />
             </div>
-        </dl>
-
-        <div>
-            <input name="form" type="submit" value="Submit" />
-        </div>
-    </form>
-</fieldset>
+            <br>
+            <div>
+                <input name="form" type="submit" value="Sign Up" />
+            </div>
+        </form>
+    </fieldset>
+</body>
