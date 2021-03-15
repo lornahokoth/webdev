efff770b1c61f2cb182e15bfa609990d7c29f7e7
lornahokoth <60718374+lornahokoth@users.noreply.github.com>
2021-03-14 20:35:05 -0400

    index.php changes. added images folder and 3 new images, including the header for the site.

--- CSC 4370 - Web Programming/HW/nerdieluv/index.php
+++ CSC 4370 - Web Programming/HW/nerdieluv/index.php
@@ -5,15 +5,25 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="nerdieluv.css" />
     <title>NerdLuv</title>
 </head>
 
 <body>
-
-Welcome!
-<a href="./signup.php"> Sign up for a new account</a>
-<a href="./matches.php"> Check your matches</a>
-
+<header id="bannerarea">
+    <img src="./images/nerd-love.png" alt="nerd love logo" />
+    <br>
+    <label>Where meek geeks meet!</label>
+</header>
+<div>
+    <h1>Welcome!</h1>
+    <div>
+        <a href="./signup.php"><img src="./images/heart.png" alt="heart" /> Sign up for a new account</a>
+    </div>
+    <div>
+        <a href="./matches.php"><img src="./images/sign-up.png" alt="sign-up" /> Check your matches</a>
+    </div>
+</div>
 <?php
  
 ?>
