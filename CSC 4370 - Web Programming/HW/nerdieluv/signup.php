e86798b9ca44ec4c79fac212e17947d93b129177
lornahokoth <60718374+lornahokoth@users.noreply.github.com>
2021-03-14 20:49:37 -0400

    formatting for signup.php

--- CSC 4370 - Web Programming/HW/nerdieluv/signup.php
+++ CSC 4370 - Web Programming/HW/nerdieluv/signup.php
@@ -7,47 +7,50 @@
     <br>
     <label>Where meek geeks meet!</label>
 </header>
-<form action="./signup-submit.php" method="POST">
-    <dl>
-        <dt>Name:</dt>
-        <div>
-            <input type="text" name="name" size="16" />
-        </div>
+<fieldset>
+    <legend>
+        New User Signup:
+    </legend>
+    <form action="./signup-submit.php" method="POST">
+        <dl>
+            <label><strong>Name:</strong></label>
+            <div>
+                <input type="text" name="name" size="16" />
+            </div>
 
-        <dt>Gender:</dt>
-        <div>
-            <input type="radio" name="gender" id="male" value="Male" />
-            <label for="male"> Male </label>
-            <input type="radio" name="gender" id="female" value="Female" />
-            <label for="female"> Female </label>
-        </div>
-        <dt>Age:</dt>
-        <div>
-            <input type="number" name="age" maxlength="2" size="6" />
-        </div>
-        <dt>Personality Type: </dt>
-        <div>
-            <input type="text" name="name" maxlength="4" size="6" />
-        </div>
-        <dt>Favorite OS:</dt>
-        <div>
-            <select name="section" id="class">
-                <option value="WS">Windows</option>
-                <option value="MS">Mac OS X</option>
-                <option value="LX">Linux</option>
+            <label><strong>Gender:</strong></label>
+            <div>
+                <input type="radio" name="gender" id="male" value="Male" />
+                <label for="male"> Male </label>
+                <input type="radio" name="gender" id="female" value="Female" />
+                <label for="female"> Female </label>
+            </div>
+            <label><strong>Age:</strong></label>
+            <div>
+                <input type="number" name="age" maxlength="2" size="6" />
+            </div>
+            <label><strong>Personality Type:</strong></label>
+            <div>
+                <input type="text" name="name" maxlength="4" size="6" />
+            </div>
+            <label><strong>Favorite OS:</strong></label>
+            <div>
+                <select name="section" id="class">
+                    <option value="WS">Windows</option>
+                    <option value="MS">Mac OS X</option>
+                    <option value="LX">Linux</option>
 
-            </select>
-        </div>
-        <dt>Seeking age:</dt>
+                </select>
+            </div>
+            <label><strong>Seeking age:</strong></label>
+                <input type="text" placeholder="min" name="minAge" maxlength="2" size="6" />
+                <label for="to"> to </label>
+                <input type="text" placeholder="max" name="maxAge" maxlength="2"  size="6" />
+            </div>
+        </dl>
+
         <div>
-
-            <input type="number" name="minAge" size="6" />
-            <label for="to"> to </label>
-            <input type="number" name="maxAge" size="6" />
+            <input name="form" type="submit" value="Submit" />
         </div>
-    </dl>
-
-    <div>
-        <input name="form" type="submit" value="Submit" />
-    </div>
-</form>
+    </form>
+</fieldset>
