f8ab04308ec947b0a74924595e3a24a36140b263
lornahokoth <60718374+lornahokoth@users.noreply.github.com>
2021-04-17 16:09:06 -0400

    Create gameplay.php
    Added skeleton of gameplay page.  This can be used for skeletons of other pages.

---
+++ CSC 4370 - Web Programming/PW/game_of_life/views/gameplay.php
@@ -0,0 +1,16 @@
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="../css/lifestylesheet.css" />
+    <script src="../js/common.js"></script>
+    <title>Game of Life</title>
+  </head>
+  <body>
+    <?php include("./header.php"); ?>
+    <div class="body">
+
+    </div>
+    <?php include("./footer.php"); ?>
+  </body>
+</html>
