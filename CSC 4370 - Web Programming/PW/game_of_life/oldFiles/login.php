0668bfa5c8112869cd4831dc793ea933b2221ca1
lornahokoth <60718374+lornahokoth@users.noreply.github.com>
2021-04-18 17:50:38 -0400

    Cleanup of files
    Moved files no longer in use to directory named oldFiles.

--- CSC 4370 - Web Programming/PW/game_of_life/oldFiles/login.php
+++ CSC 4370 - Web Programming/PW/game_of_life/oldFiles/login.php
@@ -2,7 +2,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <link rel="stylesheet" href="../css/lifestylesheet.css" />
+    <link rel="stylesheet" href="../css/stylesheet.css" />
     <script src="../js/login.js"></script>
     <script src="../js/common.js"></script>
     <title>Login</title>
